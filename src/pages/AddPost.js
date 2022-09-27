a4b1f05b490379a667cbf00fd7020c8b068a5d97
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-09-28 01:32:22 +0800

    add useState

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function AddPost(){
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [image, setImage] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleAddPost = () => {
+        alert(`
+            標題： ${title}
+            作者： ${author}
+            封面圖： ${image}
+            內容： ${content}
+        `);
+    }
 
     return (
         <div className="flex flex-col items-center">
             <Header />
             <div className="w-1/2 h-auto rounded-lg drop-shadow-md overflow-hidden bg-white p-10">
-                <div className="text-2xl font-bold mb-4">標題</div>
-                <input type="text" placeholder={"標題"} value={"標題"} onChange={()=>console.log("Hello!")} className="w-full h-10 rounded-md bg-gray-50 px-4 mb-10"></input>
+                <div className="text-2xl font-bold mb-4">
+                    標題
+                    <span className="text-base font-normal ml-6 text-gray-400">
+                        100字以內
+                    </span>
+                </div>
+                <input type="text" placeholder={"標題"} value={title} maxLength={100} onChange={(e)=>setTitle(e.target.value)} className="w-full h-10 rounded-md bg-gray-50 px-4 py-2 mb-10 focus:outline-0 focus:bg-yellow-700/5"></input>
                 <div className="text-2xl font-bold mb-4">作者</div>
-                <input type="text" placeholder={"作者"} value={"作者"} onChange={()=>console.log("Hello!")} className="w-full h-10 rounded-md bg-gray-50 px-4 mb-10"></input>
-                <div className="text-2xl font-bold mb-4">文章封面圖</div>
-                <input type="text" placeholder={"文章封面圖"} value={"文章封面圖"} onChange={()=>console.log("Hello!")} className="w-full h-10 rounded-md bg-gray-50 px-4 mb-10"></input>
+                <input type="text" placeholder={"作者"} value={author} maxLength={100} onChange={(e)=>setAuthor(e.target.value)} className="w-full h-10 rounded-md bg-gray-50 px-4 py-2 mb-10 focus:outline-0 focus:bg-yellow-700/5"></input>
+                <div className="text-2xl font-bold mb-4">
+                    文章封面圖
+                    <span className="text-base font-normal ml-6 text-gray-400">
+                        請輸入圖片網址
+                    </span>
+                </div>
+                <input type="text" placeholder={"文章封面圖"} value={image} onChange={(e)=>setImage(e.target.value)} className="w-full h-10 rounded-md bg-gray-50 px-4 py-2 mb-10 focus:outline-0 focus:bg-yellow-700/5"></input>
                 <div className="text-2xl font-bold mb-4">內容</div>
-                <input type="text" placeholder={"內容"} value={"內容"} onChange={()=>console.log("Hello!")} className="w-full h-10 rounded-md bg-gray-50 px-4 mb-10"></input>
-                
+                <textarea type="text" placeholder={"內容"} value={content} onChange={(e)=>setContent(e.target.value)} className="resize-none w-full h-80 rounded-md bg-gray-50 px-4 py-2 mb-10 focus:outline-0 focus:bg-yellow-700/5"></textarea>
             </div>
-            <button onClick={()=> null} className="mx-5 text-white font-medium px-5 py-2 bg-yellow-700/40 hover:bg-yellow-700/70 rounded-md mt-12">發布文章</button>
+            <button onClick={handleAddPost} className="mx-5 text-white font-medium px-5 py-2 bg-yellow-700/40 hover:bg-yellow-700/70 rounded-md mt-12 hover:scale-110 transition ease-in">發布文章</button>
             <Footer />
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Post from '../components/Post';
 
 export default function Home(){
+    const [keyword, setKeyword] = useState('');
+
+    const handleSearch = () => {
+        alert('搜尋： '+keyword)
+    }
+
     return (
         <div className="flex flex-col items-center">
             <Header />
             <div className="text-center text-3xl mb-8">首頁</div>
             <div className="w-2/5 h-10 rounded-md mb-12">
-                <input type="search" placeholder={"關鍵字"} value={"關鍵字"} onChange={()=>console.log("Hello!")} className="absolute w-2/5 h-10 rounded-md pl-10"></input>
+                <input type="text" placeholder={"關鍵字"} value={keyword} onChange={(e)=> setKeyword(e.target.value)} onKeyPress={(e)=> e.key === "Enter" ? handleSearch() : null } className="absolute w-2/5 h-10 rounded-md pl-10 focus:outline-0 focus:bg-yellow-700/5"></input>
                 <MagnifyingGlassIcon className="absolute h-6 w-6 my-2 ml-2"/>
             </div>
             <Post />
