0bb521a180b3d56c9dd357f66c998a8a9aa8a371
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-10-02 19:51:39 +0800

    add mongoose

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Post = require('./models/postModel');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/v1/posts', (req, res) => {
-    res.json({
-        status: 'success',
-        data: {
-            data: req.body,
-        },
-    });
+mongoose.connect('mongodb://localhost:27017/bb-log');
+
+app.post('/api/v1/posts', async (req, res) => {
+    try {
+        const post = await Post.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                data: post,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 'error',
+        });
+    }
 });
 
 app.listen(8080, () => {
