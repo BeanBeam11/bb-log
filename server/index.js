200af8b44ca7f76622ac0efae786c8a052c7ccf0
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-10-05 14:50:09 +0800

    add .env

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const Post = require('./models/postModel');
+
+dotenv.config();
 
 const app = express();
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connection successful!'));
+
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/bb-log');
 
 app.post('/api/v1/posts', async (req, res) => {
     try {
