7ab1ca5753789a6af134f30109fd8147a5a6b13a
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-10-06 00:35:51 +0800

    file structure

---
+++ server/controllers/postController.js
@@ -0,0 +1,52 @@
+const Post = require('../models/postModel');
+
+exports.getAllPost = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json({
+            status: 'success',
+            data: {
+                data: post,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 'error',
+        });
+    }
+};
+
+exports.getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                data: post,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 'error',
+        });
+    }
+};
+
+exports.createPost = async (req, res) => {
+    try {
+        const post = await Post.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                data: post,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 'error',
+        });
+    }
+};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Post = require('./models/postModel');
+
+const postRouter = require('./routes/postRoutes');
 
 dotenv.config();
 
@@ -19,56 +20,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/v1/posts', async (req, res) => {
-    try {
-        const post = await Post.create(req.body);
-        res.status(201).json({
-            status: 'success',
-            data: {
-                data: post,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.json({
-            status: 'error',
-        });
-    }
-});
-
-app.get('/api/v1/posts', async (req, res) => {
-    try {
-        const post = await Post.find();
-        res.status(200).json({
-            status: 'success',
-            data: {
-                data: post,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.json({
-            status: 'error',
-        });
-    }
-});
-
-app.get('/api/v1/posts/:id', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        res.status(200).json({
-            status: 'success',
-            data: {
-                data: post,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.json({
-            status: 'error',
-        });
-    }
-});
+app.use('/api/v1/posts', postRouter);
 
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
